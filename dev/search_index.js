053ad9bdcf8aa4fdcc00fe9811c86b2c109fd3e8
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-01 10:25:03 +0000

    build based on 2ddc34c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Codex","page":"Index","title":"Codex","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"Modules = [Codex]","category":"page"}]
+}
