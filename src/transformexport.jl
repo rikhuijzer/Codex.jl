caadfd35245fbdd76358131d33dfa734b8bb1bd4
Rik <rik@huijzer.xyz>
2020-08-02 20:19:22 +0200

    Document transform

--- src/transformexport.jl
+++ src/transformexport.jl
@@ -4,7 +4,9 @@ using Codex
 using CSV
 using DataFrames
 
-export responses
+export 
+    responses,
+    transform
 
 """
     responses(dir::String)::Dict{String,DataFrame}
@@ -17,8 +19,18 @@ function responses(dir::String)::Dict{String,DataFrame}
     names = map(rmextension, files)
     paths = map(file -> joinpath(dir, file), files)
     dfs = map(path -> CSV.File(path, delim=';') |> DataFrame!, paths)
-    # print(dfs[1])
     Dict(zip(names, dfs))
+end
+
+"""
+    transform(in_dir, out_dir; transformations::Array)
+    transform(in_dir, out_dir)
+
+Transforms an export folder, such as "2020-08", and places the files at `out_dir`.
+`transformations` is expected to be a collection of functions of type `DataFrame -> DataFrame`. 
+"""
+function transform(in_dir::String, out_dir::String; transformations)
+    mkpath(out_dir)
 end
 
 end # module
