899275818b0f640ef8539e8d246bc75ca8f8ad46
rikhuijzer <rikhuijzer@pm.me>
2020-11-10 20:19:08 +0100

    Fix test

--- src/Questionnaires/questionnaires.jl
+++ src/Questionnaires/questionnaires.jl
@@ -95,7 +95,7 @@ function responses(data_dir::String, nato_name::String, group::String; measureme
     if group == "operators"
         responses_data
     else # Graduates and dropouts.
-        # If this code takes too long, then try to reduce the number of columns.
+        # If this code takes too long (>5 seconds), then try to reduce the number of columns.
         df = @from r in responses_data begin
             @left_outer_join d in dropouts_data on r.id[7:end] equals d.id
             @where d.cohort == cohort
--- test/Questionnaires/questionnaires.jl
+++ test/Questionnaires/questionnaires.jl
@@ -8,6 +8,5 @@ using Codex.Questionnaires
     data_dir = joinpath(project_root(), "test", "data", "2020-08")
 
     df = Codex.Questionnaires.responses(data_dir, "second")
-    @test size(df) == (3, 61)
-    
+    @test size(df) == (3, 8)
 end
