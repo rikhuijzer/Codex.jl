b64e75a9384c3d33d56fd5cdc11416da0570fced
rikhuijzer <rikhuijzer@pm.me>
2020-11-10 21:51:53 +0100

    Back to English

--- src/Questionnaires/plot.jl
+++ src/Questionnaires/plot.jl
@@ -9,11 +9,11 @@ Density plot for age and domain `y` on DataFrame `df` containing `group`.
 """
 function plot_domain_density(df::DataFrame, y::Symbol)
 	xticks = y == :age ? range(0, 100, step=20) : range(0, 300, step=100) 
-    xlabel = y == :N ? "neuroticisme" :
-        y == :E ? "extraversie" :
-        y == :O ? "openheid" :
-        y == :A ? "aangenaamheid" :
-        y == :C ? "consciëntieusheid" :
+    xlabel = y == :N ? "neuroticism" :
+        y == :E ? "extraversion" :
+        y == :O ? "openness" :
+        y == :A ? "agreeableness" :
+        y == :C ? "conscientiousness" :
         string(y)
 	p = plot(df,
 		x = y, color = :group,
@@ -21,7 +21,7 @@ function plot_domain_density(df::DataFrame, y::Symbol)
 		Guide.xticks(ticks = xticks),
 		Geom.polygon(fill = false, preserve_order = true),
         Guide.xlabel(xlabel),
-		Guide.ylabel("dichtheid"),
+		Guide.ylabel("density"),
 		Theme(line_width = 0.5mm) 
 	)
 end
