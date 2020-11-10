d50debeeee3f85a9f8de59e868231e133bcb542d
rikhuijzer <rikhuijzer@pm.me>
2020-11-10 21:48:53 +0100

    Translate plot

--- src/Questionnaires/plot.jl
+++ src/Questionnaires/plot.jl
@@ -1,3 +1,5 @@
+# Loading DataFrames avoids `no default `Tables.rows` implementation for type: Gadfly.Plot`?
+using DataFrames
 using Gadfly
 
 """
@@ -7,12 +9,19 @@ Density plot for age and domain `y` on DataFrame `df` containing `group`.
 """
 function plot_domain_density(df::DataFrame, y::Symbol)
 	xticks = y == :age ? range(0, 100, step=20) : range(0, 300, step=100) 
+    xlabel = y == :N ? "neuroticisme" :
+        y == :E ? "extraversie" :
+        y == :O ? "openheid" :
+        y == :A ? "aangenaamheid" :
+        y == :C ? "consciëntieusheid" :
+        string(y)
 	p = plot(df,
 		x = y, color = :group,
 		Stat.density,
 		Guide.xticks(ticks = xticks),
 		Geom.polygon(fill = false, preserve_order = true),
-		Guide.ylabel("Density"),
+        Guide.xlabel(xlabel),
+		Guide.ylabel("dichtheid"),
 		Theme(line_width = 0.5mm) 
 	)
 end
