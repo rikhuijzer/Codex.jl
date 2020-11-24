9e654472669b72f75da610ec2b9f2eba4f1837b9
rikhuijzer <rikhuijzer@pm.me>
2020-11-24 17:49:25 +0100

    Make add GITLAB_TOKEN

--- test/Backup/gitlab.jl
+++ test/Backup/gitlab.jl
@@ -8,9 +8,9 @@ GitLab = Codex.Backup.GitLab
     # Allow tests to pass locally when GITLAB_TOKEN is not set.
     if "CI" in keys(ENV) || "GITLAB_TOKEN" in keys(ENV)
         config = GitLab.Config(ENV["GITLAB_TOKEN"], "https://gitlab.com")
-        # A mock project; only used for testing.
         project_id = 22670750
 
+        # Without this, we cannot be sure that `delete_schedule` works.
         @test GitLab.create_schedule(config, project_id, 
             (description = "Test", ref = "master", cron = "0 1 * * *")
         ).description == "Test"
@@ -19,13 +19,11 @@ GitLab = Codex.Backup.GitLab
             GitLab.delete_schedule(config, project_id, s.id)
         end
 
-        # Verify that all schedules are deleted.
         @test length(GitLab.list_schedules(config, project_id)) == 0
 
-        # Add a new schedule.
         @test GitLab.create_schedule(config, project_id, 
             (description = string(Dates.now()), ref = "master", 
-            cron = "0 1 * * *", active = "false")
+                cron = "0 1 * * *", active = "false")
         ).active == false
 
         @test length(GitLab.list_schedules(config, project_id)) == 1
