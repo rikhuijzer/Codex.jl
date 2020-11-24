b1ba86a57162a8cae63d83318cff373c1a7933ad
rikhuijzer <rikhuijzer@pm.me>
2020-11-24 20:28:14 +0100

    Add structs for GitLab

--- src/gitlab.jl
+++ src/gitlab.jl
@@ -6,78 +6,103 @@ using Codex
 using HTTP
 using HTTP: Response
 
-## TODO: Assert response.params == params. 
-
 struct Config
     token::AbstractString
     url::AbstractString
 end
 
+struct Project
+    config::Config
+    project_id::Int
+end
+project_url(p::Project) = 
+    "$(p.config.url)/api/v4/projects/$(p.project_id)"
+
+struct Schedule
+    project::Project
+    schedule_id::Int
+end
+schedule_url(s::Schedule) = 
+    "$(project_url(s.project))/pipeline_schedules/$(s.schedule_id)"
+
+struct Variable
+    schedule::Schedule
+    key::AbstractString
+end
+variable_url(v::Variable) = 
+    "$(schedule_url(v.schedule))/variables/$(v.key)" 
+
 auth_header(config::Config) = Dict("PRIVATE-TOKEN" => config.token)
-# Make all values string
+auth_header(p::Project) = auth_header(p.config)
+auth_header(s::Schedule) = auth_header(s.project)
+auth_header(v::Variable) = auth_header(v.schedule)
+
 # All values are coverted to string to avoid errors when using `active = false`.
-form(params) = HTTP.Form(nt2dict(apply(string, params)))
+form(param::NamedTuple) = HTTP.Form(nt2dict(apply(string, param)))
 json(r::Response) = JSON2.read(String(r.body))
 
 """
-    list_schedules(config::Config, project_id::Int) -> Vector{NamedTuple}
+    list_schedules(p::Project) -> Vector{NamedTuple}
 
 Returns an array of named tuples `(id = ..., description = ..., ...)`.
-For `params`, see
+For details, see
 <https://docs.gitlab.com/ee/api/pipeline_schedules.html#get-all-pipeline-schedules>.
 """
-function list_schedules(config::Config, project_id::Int)::Vector{NamedTuple}
-    r = HTTP.request("GET",
-        "$(config.url)/api/v4/projects/$project_id/pipeline_schedules",
-        auth_header(config)
-    )
+function list_schedules(p::Project)::Vector{NamedTuple}
+    endpoint = "$(project_url(p))/pipeline_schedules"
+    r = HTTP.request("GET", endpoint, auth_header(p))
     list = json(r)
 end
-n_schedules(config, project_id) = length(GitLab.list_schedules(config, project_id))
+n_schedules(p::Project) = length(GitLab.list_schedules(p))
 
 """
-    create_schedule(config::Config, project_id::Int, params::NamedTuple) -> NamedTuple
+    create_schedule(p::Project, param::NamedTuple) -> NamedTuple
 
-For `params`, see 
+For `param`, see 
 <https://docs.gitlab.com/ee/api/pipeline_schedules.html#create-a-new-pipeline-schedule>.
 """
-function create_schedule(config::Config, project_id::Int, params::NamedTuple)::NamedTuple
-    r = HTTP.request("POST",
-        "$(config.url)/api/v4/projects/$project_id/pipeline_schedules",
-        auth_header(config),
-        form(params)
-    )
+function create_schedule(p::Project, param::NamedTuple)::NamedTuple
+    endpoint = "$(project_url(p))/pipeline_schedules"
+    r = HTTP.request("POST", endpoint, auth_header(p), form(param))
     nt = json(r)
-    for key in keys(params)
-        @assert params[key] == nt[key] "$key: $(params[key]) != $(nt[key])"
+    for key in keys(param)
+        @assert param[key] == nt[key] "$key: $(param[key]) != $(nt[key])"
     end
     nt
 end
 
 """
-    delete_schedule(config::Config, project_id::Int, schedule_id::Int) -> NamedTuple
+    delete_schedule(s::Schedule) -> NamedTuple
 
 Deletes `schedule_id` for `project_id`.
 """
-function delete_schedule(config::Config, project_id::Int, schedule_id::Int)::NamedTuple
-    r = HTTP.request("DELETE",
-        "$(config.url)/api/v4/projects/$project_id/pipeline_schedules/$schedule_id",
-        auth_header(config)
-    )
+function delete_schedule(s::Schedule)::NamedTuple
+    r = HTTP.request("DELETE", schedule_url(s), auth_header(s))
     json(r)
 end
 
 """
-    enforce_schedules(config::Config, project_id::Int, params::Vector) -> Vector{NamedTuple}
+    enforce_schedules(p::Project, params::Vector) -> Vector{NamedTuple}
 
 Enforce schedules for `project_id` as defined by `params`.
 For example, `params = [(description = "A", ...), (description = "B", ...)]`.
 """
-function enforce_schedules(config::Config, project_id::Int, params::Vector)::Vector{NamedTuple}
-    schedules = list_schedules(config, project_id)
-    [delete_schedule(config, project_id, s.id) for s in schedules]
-    @assert n_schedules(config, project_id) == 0
-    [create_schedule(config, project_id, p) for p in params]
+function enforce_schedules(p::Project, params::Vector)::Vector{NamedTuple}
+    list = list_schedules(p)
+    [delete_schedule(Schedule(p, nt.id)) for nt in list]
+    @assert n_schedules(p) == 0
+    [create_schedule(p, param) for param in params]
+end
+
+"""
+    edit_schedule_variable(v::Variable, param::NamedTuple) -> NamedTuple
+
+"""
+function edit_schedule_variable(v::Variable, param::NamedTuple)::NamedTuple
+    r = HTTP.request("PUT", variable_url(v), auth_header(v), form(param))
+    nt = json(r)
+    @assert param[:value] == nt[:value]
+    nt
 end
 
 end # module
--- test/gitlab.jl
+++ test/gitlab.jl
@@ -9,23 +9,17 @@ GitLab = Codex.GitLab
     if "CI" in keys(ENV) || "GITLAB_TOKEN" in keys(ENV)
         config = GitLab.Config(ENV["GITLAB_TOKEN"], "https://gitlab.com")
         project_id = 22670750
+        project = GitLab.Project(config, project_id)
 
-        # Without this, we cannot be sure that `delete_schedule` works.
-        @test GitLab.create_schedule(config, project_id, 
+        # Ensure that `enforce_schedule` also deletes.
+        @test GitLab.create_schedule(project,
             (description = "Test", ref = "master", cron = "0 1 * * *")
         ).description == "Test"
 
-        for s in GitLab.list_schedules(config, project_id)
-            GitLab.delete_schedule(config, project_id, s.id)
-        end
-        @test GitLab.n_schedules(config, project_id) == 0
-
         params = (description = string(Dates.now()), ref = "master", 
-            cron = "0 1 * * *", active = false)
-        @test GitLab.create_schedule(config, project_id, params).active == false
-        @test GitLab.n_schedules(config, project_id) == 1
-
-        GitLab.enforce_schedules(config, project_id, [params, params]) 
-        @test GitLab.n_schedules(config, project_id) == 2
+            cron = "0 2 * * *", active = false)
+        schedules = GitLab.enforce_schedules(project, [params, params]) 
+        @test GitLab.n_schedules(project) == 2
+        @test schedules[1].active == false
     end
 end
