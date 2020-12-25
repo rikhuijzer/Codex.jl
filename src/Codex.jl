adfe89db9ef5246ab157f97d49840cad04ad4916
Rik <rikhuijzer@pm.me>
2020-12-25 15:35:42 +0100

    Pin DataFrames to 0.22

--- src/Codex.jl
+++ src/Codex.jl
@@ -7,5 +7,6 @@ include("df-transformations.jl")
 include(joinpath("Backup", "backup.jl"))
 include(joinpath("TransformExport", "transformexport.jl"))
 include(joinpath("Questionnaires", "questionnaires.jl"))
+include("comparing-means-sds.jl")
 
 end # module
