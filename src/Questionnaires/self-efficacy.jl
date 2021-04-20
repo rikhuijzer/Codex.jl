0f0ed243fdec61d0d76ae63399318ea69c8b16a8
Rik Huijzer <rikhuijzer@pm.me>
2021-04-20 12:02:11 +0200

    Add comment

--- src/Questionnaires/self-efficacy.jl
+++ src/Questionnaires/self-efficacy.jl
@@ -3,6 +3,8 @@
 
 Return the scores for the echo questionnaire, which is about self-efficacy.
 None of the items appear to be reversed, so this method just returns the sum.
+
+This function assumes questions [1, 14] which holds for all questionnaires in from 2018 to the time of writing (2021).
 """
 function self_efficacy2scores(echo::DataFrame)::DataFrame
     df = disallowmissing(echo)
