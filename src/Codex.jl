48aa7bf4ea50f711120bbeb5f855f5e11cd1ee35
Rik Huijzer <rikhuijzer@pm.me>
2021-04-30 20:14:04 +0200

    Remove order_with

--- src/Codex.jl
+++ src/Codex.jl
@@ -3,7 +3,6 @@ module Codex
 include("gitlab.jl")
 include("utils.jl")
 include("stats.jl")
-include("df-transformations.jl")
 include(joinpath("Backup", "backup.jl"))
 include(joinpath("TransformExport", "transformexport.jl"))
 include(joinpath("Questionnaires", "questionnaires.jl"))
