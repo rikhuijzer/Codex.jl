d5607132bc7df33527333a7e7e1a7435baa362ff
rikhuijzer <rikhuijzer@pm.me>
2022-04-05 13:46:27 +0200

    Add motivation type

--- src/Questionnaires/hotel.jl
+++ src/Questionnaires/hotel.jl
@@ -1,6 +1,39 @@
 # Hotel is a combination of many questionnaires:
 # Mindset [Dweck, 2013], fear of failure [Thrash & Elliot, 2003], basic motives [van Yperen et al., 2014], motivation type [Pelletier et al., 2013], and approach-avoidance temperament [Elliot & Thrash, 2010]
 
+"""
+    add_motivation_type!(df::DataFrame)::DataFrame
+
+Add type motivatie columns and remove the items.
+
+Scoring is based on Ruud his Word document:
+```
+Amotivation (3,11,16);
+External regulation (4,8,13);
+Introjected regulation (7,12,18);
+Identified regulation (5,9,15);
+Integrated regulation (1,6,17);
+Intrinsic motivation (2,10,14)
+```
+
+The items start at 48 for all versions.
+"""
+function add_motivation_type!(df::DataFrame)::DataFrame
+    questions = Dict(
+        :amotivation => [3, 11, 16],
+        :external_regulation => [4, 8, 13],
+        :introjected_regulation => [7, 12, 18],
+        :identified_regulation => [5, 9, 15],
+        :integrated_regulation => [1, 6, 17],
+        :intrinsic_motivation => [2, 10, 14]
+    )
+    for key in keys(questions)
+        Q = ["v$(43 + i)" for i in questions[key]]
+        dropmissing!(df, Q)
+        select!(df, Q => ByRow(+) => key, Not(Q))
+    end
+    return df
+end
 
 """
     add_temperaments!(df)
@@ -18,12 +51,12 @@ Also for 2020 first.
 """
 function add_temperaments!(df)
     let
-        Q = ["v$(i + 61)" for i in [2, 4, 5, 8, 10, 11]]
+        Q = ["v$(61 + i)" for i in [2, 4, 5, 8, 10, 11]]
         dropmissing!(df, Q)
         select!(df, Q => ByRow(+) => :approach_temperament, Not(Q))
     end
     let
-        Q = ["v$(i + 61)" for i in [1, 3, 6, 7, 9, 12]]
+        Q = ["v$(61 + i)" for i in [1, 3, 6, 7, 9, 12]]
         dropmissing!(df, Q)
         select!(df, Q => ByRow(+) => :avoidance_temperament, Not(Q))
     end
@@ -32,6 +65,7 @@ end
 
 function hotel2scores(hotel::DataFrame)::DataFrame
     df = copy(hotel)
+    add_motivation_type!(df)
     add_temperaments!(df)
     return df
 end
